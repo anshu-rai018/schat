76b50e3bcf3d677cba574a0ef7fc8e04968acbe5
Anshu Rai <anshu.rai018@gmail.com>
2024-05-06 23:27:48 +0530

    login and signup update

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,18 +1,21 @@
 import React from 'react'
 import Image from 'next/image'
+import MyAi from "@/public/myai-asset.png"
+import { Button } from './ui/button'
+import { FaLaptop } from 'react-icons/fa';
 
 const Header = () => {
   return (
-    <div>
+    <div className='flex justify-between items-center max-w-6xl mx-auto'>
 
       <div>
-        <h1 className='text-7xl font-medium'>Snapchat is <br/>now on the<br/> web!</h1>
-        <h1 className='my-5 text-xl'>Chat, Snap, and video call your friends from <br/> wherever you are.</h1>
-
+        <h1 className='text-7xl font-medium'>Snapchat is <br />now on the<br /> web!</h1>
+        <h1 className='my-5 text-xl'>Chat, Snap, and video call your friends from <br /> wherever you are.</h1>
+        <Button className='gap-2 rounded-full'><FaLaptop/> Login to chat</Button>
       </div>
-      
+
       <div>
-<Image src={MyAi} alt="myai" height={650}  width={700}/>
+        <Image src={MyAi} alt="myai" width={650} height={650} />
       </div>
 
     </div>
