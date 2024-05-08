df83c167c25f4afb2db20176d2dee440a627c1a5
Anshu Rai <anshu.rai018@gmail.com>
2024-05-08 22:51:30 +0530

    auth setup

---
+++ auth.ts
@@ -0,0 +1,26 @@
+import { profile } from "console";
+import NextAuth from "next-auth"
+import Github from "next-auth/providers/github";
+import connectDatabase from "./lib/db";
+ 
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers: [
+    Github({
+        clientId:process.env.NEXT_GITHUB_CLIENT_ID,
+        clientSecret:process.env.NEXT_GITHUB_CLIENT_SECRET
+    })
+  ],
+  secret:process.env.NEXTAUTH_SECRET,
+
+  callbacks:{
+    async signIn({account, profile}) {
+        if(account?.provider==='github'){
+            await connectDatabase();
+            try{
+                const await
+            } catch(error){}
+        }
+        
+    }
+  }
+})
---
+++ models/user.model.ts
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const userModel = new mongoose.Schema({
+    username:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    fullname:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    profilePhoto:{
+        type:String,
+        default:""
+    }
+},{timestamps:true});
