824e87b20382fbb2c128b24d7280dbe660da6949
Anshu Rai <anshu.rai018@gmail.com>
2024-06-25 01:18:38 +0530

    first changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images:{
-        remotePatterns:[
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+    images: {
+        remotePatterns: [
             {
-                protocol:'https',
-                hostname:'w7.pngwing.com'
+                protocol: 'https',
+                hostname: 'w7.pngwing.com'
             },
             {
-                protocol:'https',
-                hostname:'res.cloudinary.com'
+                protocol: 'https',
+                hostname: 'res.cloudinary.com'
             },
         ]
     },
-    experimental:{
-        serverActions:{
-            bodySizeLimit:'2mb'
+    experimental: {
+        serverActions: {
+            bodySizeLimit: '2mb'
         }
     }
 };
